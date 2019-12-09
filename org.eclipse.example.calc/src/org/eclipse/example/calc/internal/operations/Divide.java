e0820430598a1440ec9994c45b29f89858d7da9e
Jan Christoph Marxen <jan_christoph.marxen@fiduciagad.de>
2019-12-09 14:12:08 +0100

    Divide implemented

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
@@ -0,0 +1,20 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+public class Divide extends AbstractOperation implements BinaryOperation {
+
+	@Override
+	public String getName() {
+		return "/";
+	}
+
+	@Override
+	public float perform(float arg1, float arg2) {
+		if(arg1 == 0 || arg2 == 0) {
+			return 0;
+		}
+		return arg1 / arg2;
+	}
+
+}
