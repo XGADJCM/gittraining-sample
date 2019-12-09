5c7e6858c6426054371c1258008e6445c1dc208f
Jan Christoph Marxen <jan_christoph.marxen@fiduciagad.de>
2019-12-09 16:41:31 +0100

    Interactive Rebase

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -7,7 +7,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * xgadjcm
+ * xgadjcm - B
  *******************************************************************************/
 package org.eclipse.example.calc.internal;
 
